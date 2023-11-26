84e5ed68889b15f431f8a1a017d1cb4ccae92e2b
habberrih <abdul2013ly@gmail.com>
2023-11-26 20:09:55 +0200

    refactor: add app bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../utils/colors.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,7 +13,29 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('ToDo Application'),
+        backgroundColor: colorBG,
+        elevation: 0,
+        title: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              const Icon(
+                Icons.menu,
+                color: colorBlack,
+                size: 30,
+              ),
+              SizedBox(
+                height: 40,
+                width: 40,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20.0),
+                  child: Image.asset(''),
+                ),
+              )
+            ],
+          ),
+        ),
         centerTitle: true,
         titleSpacing: 1.0,
       ),
