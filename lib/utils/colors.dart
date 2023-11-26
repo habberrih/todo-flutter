5831e4e2f841c96cdd0f1c9a627dab9e6a5ce543
habberrih <abdul2013ly@gmail.com>
2023-11-26 19:57:52 +0200

    feat: add app colors

---
+++ lib/utils/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color primaryRedColor = Color(0xFFDA4040);
+const Color colorBlue = Color(0xFF5F52EE);
+
+const Color colorBlack = Color(0xFF3A3A3A);
+const Color colorGrey = Color(0xFF717171);
+
+const Color colorBG = Color(0xFFEEEFF5);
