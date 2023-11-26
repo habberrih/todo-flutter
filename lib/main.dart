7d46bd761981a2d4ba0107ba4c6d75d739855aa0
habberrih <abdul2013ly@gmail.com>
2023-11-26 19:58:27 +0200

    refactor: add device_preview feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,22 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import './pages/home_page.dart';
 
 void main() {
-  runApp();
+  runApp(DevicePreview(
+    enabled: true,
+    builder: (BuildContext context) => const HomeApp(),
+  ));
+}
+
+class HomeApp extends StatelessWidget {
+  const HomeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
 }
