90de6c47eee4d24eb3847e8d669fc1a9351e8314
habberrih <abdul2013ly@gmail.com>
2023-11-27 11:53:47 +0400

    refactor: update status bar color

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import './pages/home_page.dart';
 
 void main() {
@@ -14,9 +15,13 @@ class HomeApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+    ));
+    return  const MaterialApp(
       home: HomePage(),
       debugShowCheckedModeBanner: false,
+      
     );
   }
 }
